a0a756bc18e9b0725c152dbc69b0e60d1c7008a4
Kier Borromeo <seraphipod@gmail.com>
2023-06-11 21:29:05 +0800

    Skip TS

--- next.config.mjs
+++ next.config.mjs
@@ -25,5 +25,13 @@ const config = {
   experimental: {
     esmExternals: false,
   },
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 export default config;
