afd224ddf7b7af4282975a4ba58da743bf6df94e
Kier Borromeo <seraphipod@gmail.com>
2023-06-12 10:19:59 +0800

    Login page improvement

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,39 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useSession, signIn } from "next-auth/react";
+import Image from "next/image";
+import { signIn } from "next-auth/react";
 import { config } from "~/config";
 import { Button } from "~/ui-components";
 import { getGuestServerSideProps } from "~/server/auth";
+import login from "~/assets/login.png";
 
 const Login: NextPage = () => {
-  const { data: sessionData } = useSession();
-
   return (
     <>
       <Head>
         <title>{config.app.title} - Login</title>
       </Head>
 
-      <div className="flex h-screen bg-neutral-900 font-sans text-white">
-        <div className="w-1/2 bg-neutral-300"></div>
+      <div className="flex h-screen bg-neutral-900 font-sans">
+        <div className="relative w-1/2 overflow-hidden bg-neutral-300">
+          <Image src={login} alt="tite" fill className="z-40" />
 
-        <div className="h-full w-1/2">
+          <div className="relative z-50 h-full">
+            <div className="p-10">
+              <h1 className="inline border-2 border-black p-2 font-bold uppercase leading-none text-black">
+                M
+              </h1>
+            </div>
+
+            <div className="absolute bottom-0 left-0 z-50 p-10">
+              <div className="text-2xl text-black">
+                &mdash; Anything worth doing, is worth doing poorly.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-full w-1/2 text-white">
           <div className="flex h-full items-center justify-center py-4 px-4">
             <div className="w-[240px]">
               Welcome Back
@@ -25,6 +41,8 @@ const Login: NextPage = () => {
               <Button full variant="primary" onClick={() => signIn()}>
                 Login with Discord
               </Button>
+              <div className="mb-[320px]"></div>
+              <div className="text-center">By Joyce and Kier</div>
             </div>
           </div>
         </div>
